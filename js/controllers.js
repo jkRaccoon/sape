722db639710c61fecbc68ea1487674d39157bb04
JikwangKim <jikwangkim@JikwangKimui-iMac.local>
2015-10-29 19:17:09 +0900

    마크업 2차. (코스) 미완

--- js/controllers.js
+++ js/controllers.js
@@ -1,30 +1,8 @@
 angular.module('starter.controllers', [])
 
 //라이딩 리스트
-.controller('RideList', function($scope, $ionicLoading, $compile, $stateParams, $ionicPopup,Course) {
+.controller('RideList', function($scope, $ionicLoading, $compile, $stateParams, $ionicPopup,$state,Course) {
 	
-	function initialize() {
-		var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-		var options = { //지도를 생성할 때 필요한 기본 옵션
-			center: new daum.maps.LatLng(37.51186511, 126.99830338718), //지도의 중심좌표.
-			level: 3, //지도의 레벨(확대, 축소 정도)
-			
-		};
-		
-		var map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴
-		
-		map.addOverlayMapTypeId(daum.maps.MapTypeId.BICYCLE);
-		
-		daum.maps.event.addListener(map, 'click', function(mouseEvent) {
-		    var latlng = mouseEvent.latLng;
-		   
-		});
-
-		$scope.map = map;
-		
-		
-    }
-    
    
     $scope.courseId = $stateParams.courseId; //코스아이디
     
@@ -56,74 +34,30 @@ angular.module('starter.controllers', [])
 			        		  }
 			        	  }
 			          }
-			          ]
-		  });
+			        ]
+		});
 		myPopup.then(function(res) {
 			
 		});
 	
 	};
-	if(!$scope.map) {
-		initialize();
+	
+	$scope.enterRideRoom = function(courseId , courseDetailId){
+	
+		$state.go('tab.course-detail',{courseId:courseId , courseDetailId:courseDetailId});
 	}
 	
+	$scope.changeDirection = function(){
+		$scope.reverse = ($scope.reverse == true)?false:true;
+		
+	}
 	
 	Course.routeLiveList($scope.courseId).success(function(result){
+		console.log(result)
 		$scope.courseList = result;
 	});
 	
 	
-	Course.route($scope.courseId).success(function(result){
-		
-		var linePath = new Array();
-		var bounds = new daum.maps.LatLngBounds();
-		var point = null;
-		
-		for(var i in result){
-			point = new daum.maps.LatLng(result[i]["lat"], result[i]["lng"]);
-			linePath.push(point);
-			bounds.extend(point);
-		}
-		
-		// 지도에 표시할 선을 생성합니다
-		var polyline = new daum.maps.Polyline({
-		    path: linePath, // 선을 구성하는 좌표배열 입니다
-		    strokeWeight: 5, // 선의 두께 입니다
-		    strokeColor: '#0000FF', // 선의 색깔입니다
-		    strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-		    strokeStyle: 'solid' // 선의 스타일입니다
-		});
-		
-		
-		
-		var startCircle = new daum.maps.Circle({
-		    center : linePath[0],  // 원의 중심좌표 입니다 
-		    radius: 50, // 미터 단위의 원의 반지름입니다 
-		    strokeWeight: 2, // 선의 두께입니다 
-		    strokeColor: '#75B8FA', // 선의 색깔입니다
-		    strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-		    strokeStyle: 'solid', // 선의 스타일 입니다
-		    fillColor: '#CFE7FF', // 채우기 색깔입니다
-		    fillOpacity: 0.7  // 채우기 불투명도 입니다   
-		});
-		
-		startCircle.setMap($scope.map); 
-		
-		var endCircle = new daum.maps.Circle({
-		    center : linePath[linePath.length-1],  // 원의 중심좌표 입니다 
-		    radius: 50, // 미터 단위의 원의 반지름입니다 
-		    strokeWeight: 2, // 선의 두께입니다 
-		    strokeColor: '#FA75B8', // 선의 색깔입니다
-		    strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-		    strokeStyle: 'solid', // 선의 스타일 입니다
-		    fillColor: '#FFCFE7', // 채우기 색깔입니다
-		    fillOpacity: 0.7  // 채우기 불투명도 입니다   
-		});
-		endCircle.setMap($scope.map); 
-		polyline.setMap($scope.map); 
-		$scope.map.setBounds(bounds,10,10,10,10);
-
-	});
 	
 	
 
